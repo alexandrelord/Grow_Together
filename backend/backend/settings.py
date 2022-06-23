9c32654212fd6fde79bf53ce09700770a32ae2be
ale lord <j.alexlord@gmail.com>
2022-06-23 10:30:33 -0500

    setup models and database connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grow_together',
     }
 }
 
--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from psycopg2 import Timestamp
 
-# Create your models here.
+class Plant(models.Model):
+    scientific_name = models.CharField(max_length=100)
+    common_name = models.CharField(max_length=100)
+    water_use = models.CharField(max_length=50)
+    light = models.CharField(max_length= 50) 
+    maintenance = models.CharField(max_length=50)
+    image = models.CharField(max_length=200, blank=True, default=None)
+
+    def __str__(self):
+        return self.scientific_name
+
+class UserPlant(models.Model):
+   user= models.ForeignKey(User, on_delete=models.CASCADE, default= 1)
+   user_plant = models.ForeignKey(Plant, related_name="user_plant", on_delete=models.CASCADE, blank=True, default=None)
+   matched_plant = models.ForeignKey(Plant, related_name="matched_plant", on_delete=models.CASCADE,  blank=True, default=None)
+   plant_image = models.CharField(max_length=200, blank=True, default=None)
+
+   def __str__(self):
+       return self.plant_image
