2c57fd1eb216f6ddb3dec42c1b7da39ebbf7f45a
ale lord <j.alexlord@gmail.com>
2022-06-23 12:22:18 -0500

    setting up API

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    # path('', views.home),
+    path('api/plants/', views.plant_list),
+    path('api/plants/<int:id>', views.plant_detail),
 ]
