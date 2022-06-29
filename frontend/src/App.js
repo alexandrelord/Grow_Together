0cf24dab127f8958e82d6ae89a382348cf031664
ale lord <j.alexlord@gmail.com>
2022-06-29 14:50:23 -0500

    add logout functionality frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import { useState, useEffect} from 'react'
-import { getUser } from './utilities/users-services'
 import { Link } from 'react-router-dom'
+import { getUser } from './utilities/users-services'
+import { logout } from './utilities/users-api';
 import AuthPage from './pages/AuthPage/AuthPage'
 import PlantList from './components/PlantList/PlantList'
 
 export default function App() {
   const [user, setUser] = useState(getUser())
+
+  async function handleLogout(){
+    try {
+      const response = await logout()
+      setUser(getUser())
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
     return (
       <div className='App'>
@@ -14,6 +24,7 @@ export default function App() {
         <div>
         <h1>Logged in</h1>
         <PlantList></PlantList>
+        <button onClick={handleLogout}>Logout</button>
         </div>
         :<AuthPage setUser={setUser}/>}
       </div>
--- frontend/src/utilities/users-api.js
+++ frontend/src/utilities/users-api.js
@@ -13,6 +13,14 @@ export function login(credentials) {
      return obtainToken(credentials)
 }
 
+export function logout() {
+    const response = axiosInstance.post('/authentication/blacklist/', {'refresh_token': localStorage.getItem('refresh_token')})
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('refresh_token')
+    axiosInstance.defaults.headers['Authorization'] = null
+    return response
+}
+
 export async function obtainToken(credentials) {
     const token = await axiosInstance.post('/authentication/token/obtain/', credentials)
     axiosInstance.defaults.headers['Authorization'] = `JWT ${token.data.access}`
