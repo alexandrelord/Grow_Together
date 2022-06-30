149e557caa1e4beee2af4e9528ab45010ec65e63
ishan <ishan.khosla22@gmail.com>
2022-06-30 13:19:27 -0400

    fix webscrape fields

--- backend/api/management/commands/scrape.py
+++ backend/api/management/commands/scrape.py
@@ -11,7 +11,7 @@ baseURL = "http://www.tropicopia.com/house-plant/detail.np/detail-"
      
 class Command(BaseCommand):
     def handle(self, *args, **options):
-       for num in range (152,153):
+       for num in range (1,356):
         newNum = str(num)
         if len(newNum) == 1:
             newNum = ("0" + newNum)
@@ -52,6 +52,67 @@ class Command(BaseCommand):
                         light = newLight,
                         insects = newInsects,
                         disease = newDisease,
+                        image = image
+                    )
+            elif light == 'Light tolered :':
+                otherLight = info[33].text.strip()
+                otherLight = otherLight.split('(')
+                otherLight = otherLight[0]
+                otherWater = info[37].text.strip()
+                otherWater = otherWater.split('&')
+                otherWater = otherWater[0]
+                otherInsects = info[39].text.strip()
+                otherDisease = info[41].text.strip()
+                if Plant.objects.filter(scientific_name = name):
+                    continue
+                else:
+                    Plant.objects.create(
+                        scientific_name = name,
+                        common_name = common, 
+                        water_use = otherWater,
+                        light = otherLight,
+                        insects = otherInsects,
+                        disease = otherDisease,
+                        image = image
+                    )
+            elif common == 'Common name (fr.) :':
+                unknownCommon = 'N/A'
+                unknownCommonlight = info[32].text.strip()
+                unknownCommonlight = unknownCommonlight.split('(')
+                unknownCommonlight = unknownCommonlight[0]
+                unknownCommonWater = info[36].text.strip()
+                unknownCommonWater = unknownCommonWater.split('&')
+                unknownCommonWater = unknownCommonWater[0]
+                unknownCommonInsects = info[38].text.strip()
+                unknownCommonDisease = info[40].text.strip()
+                if Plant.objects.filter(scientific_name = name):
+                    continue
+                else:
+                    Plant.objects.create(
+                        scientific_name = name,
+                        common_name = unknownCommon, 
+                        water_use = unknownCommonWater,
+                        light = unknownCommonlight,
+                        insects = unknownCommonInsects,
+                        disease = unknownCommonDisease,
+                        image = image
+                    )
+            elif water == 'Insects :':
+                altWater = info[37].text.strip()
+                altWater = altWater.split('&')
+                altWater = altWater[0]
+                altInsects = info[39].text.strip()
+                altDisease = info[41].text.strip()
+                if Plant.objects.filter(scientific_name = name):
+                    continue
+                else:
+                    Plant.objects.create(
+                        scientific_name = name,
+                        common_name = common, 
+                        water_use = altWater,
+                        light = light,
+                        insects = altInsects,
+                        disease = altDisease,
                         image = image
                     )
             else:
@@ -106,6 +167,67 @@ class Command(BaseCommand):
                         light = newLight,
                         insects = newInsects,
                         disease = newDisease,
+                        image = image
+                    )
+            elif light == 'Light tolered :':
+                otherLight = info[33].text.strip()
+                otherLight = otherLight.split('(')
+                otherLight = otherLight[0]
+                otherWater = info[37].text.strip()
+                otherWater = otherWater.split('&')
+                otherWater = otherWater[0]
+                otherInsects = info[39].text.strip()
+                otherDisease = info[41].text.strip()
+                if Plant.objects.filter(scientific_name = name):
+                    continue
+                else:
+                    Plant.objects.create(
+                        scientific_name = name,
+                        common_name = common, 
+                        water_use = otherWater,
+                        light = otherLight,
+                        insects = otherInsects,
+                        disease = otherDisease,
+                        image = image
+                    )
+            elif common == 'Common name (fr.) :':
+                unknownCommon = 'N/A'
+                unknownCommonlight = info[32].text.strip()
+                unknownCommonlight = unknownCommonlight.split('(')
+                unknownCommonlight = unknownCommonlight[0]
+                unknownCommonWater = info[36].text.strip()
+                unknownCommonWater = unknownCommonWater.split('&')
+                unknownCommonWater = unknownCommonWater[0]
+                unknownCommonInsects = info[38].text.strip()
+                unknownCommonDisease = info[40].text.strip()
+                if Plant.objects.filter(scientific_name = name):
+                    continue
+                else:
+                    Plant.objects.create(
+                        scientific_name = name,
+                        common_name = unknownCommon, 
+                        water_use = unknownCommonWater,
+                        light = unknownCommonlight,
+                        insects = unknownCommonInsects,
+                        disease = unknownCommonDisease,
+                        image = image
+                    )
+            elif water == 'Insects :':
+                altWater = info[37].text.strip()
+                altWater = altWater.split('&')
+                altWater = altWater[0]
+                altInsects = info[39].text.strip()
+                altDisease = info[41].text.strip()
+                if Plant.objects.filter(scientific_name = name):
+                    continue
+                else:
+                    Plant.objects.create(
+                        scientific_name = name,
+                        common_name = common, 
+                        water_use = altWater,
+                        light = light,
+                        insects = altInsects,
+                        disease = altDisease,
                         image = image
                     )
             else:
