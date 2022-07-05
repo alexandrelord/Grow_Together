c87aa7a4ca60a0ab9e6e9eff4760545c4a0c173e
ishan <ishan.khosla22@gmail.com>
2022-07-05 12:53:18 -0400

    implent plantNet API and S3

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,34 +1,69 @@
-import { useContext, useState } from 'react'
-import style from './Home.module.css'
-import Button from '../Reusables/Button/Button'
-import AuthContext from '../../context/AuthProvider'
-import useRefreshToken from '../../hooks/useRefreshToken'
+import { useContext, useState } from "react";
+import style from "./Home.module.css";
+import Button from "../Reusables/Button/Button";
+import AuthContext from "../../context/AuthProvider";
+import useRefreshToken from "../../hooks/useRefreshToken";
+import axios from 'axios';
+import S3 from 'react-aws-s3';
+window.Buffer = window.Buffer || require('buffer').Buffer;
 
 export default function Home() {
-    // const { setAuth } = useContext(AuthContext)
-    const refresh = useRefreshToken()
-  
-    return (
-        <section>
-            <h1>Home</h1>
-            <button onClick={() => refresh()}>Refresh</button>
-        </section>
-    )
+  // const { setAuth } = useContext(AuthContext)
+  const [image, setImage] = useState("");
+  const [plant, setPlant] = useState(null);
+  const [plantimg, setPlantImg] = useState(null);
+  const config = {
+    bucketName: "grow-together",
+    region: "us-west-1",
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  };
+  const ReactS3Client = new S3(config);
+  const S3_BASE_URL = "https://s3-us-west-1.amazonaws.com/";
+  const BUCKET = "grow-together/";
+  const refresh = useRefreshToken();
+
+  function handleChange(e) {
+    setImage(e.target.files[0]);
+  }
+
+  async function handleClick(e) {
+    e.preventDefault();
+    let key = "";
+    await ReactS3Client.uploadFile(image).then((data) => (key = data.key));
+    let api_url = "/v2/identify/all?api-key=";
+    let api_key = "2b10P60RFzvdu9lsD1dWCHuk6u";
+    let organ_1 = "organs=flower";
+    let s3Url = S3_BASE_URL + BUCKET + key;
+    setPlantImg(s3Url);
+    let encodedURL = encodeURIComponent(s3Url);
+    let url = api_url + api_key + "&images=" + encodedURL + "&" + organ_1;
+    axios.get(url).then((res) => {
+      setPlant(res.data);
+      console.log(res.data)
+    });
+  }
+
+  return (
+    //         <section>
+    //             <h1>Home</h1>
+    //             <button onClick={() => refresh()}>Refresh</button>
+    //         </section>
+    //     )
+    // }
+
+    <div className={style.card} /* onSubmit={handleSubmit} */>
+      <div className={style.image}>
+        <p>Looking for a plant to match yours?</p>
+        <p>Take a photo and find a match!</p>
+      </div>
+      <div className={style.addPhoto}>
+        <form onSubmit={handleClick}>
+          <label>Select File</label>
+          <input onChange={handleChange} type="file" name="photo" />
+          <Button label="Upload" type="submit">Submit</Button>
+        </form>
+      </div>
+    </div>
+  );
 }
-
-
-
-
-// <div className={style.card} /* onSubmit={handleSubmit} */ >
-    //     <div className={style.image}>
-    //         <p>Looking for a plant to match yours?</p>
-    //         <p>Take a photo and find a match!</p>
-    //     </div>
-    //     <div className={style.addPhoto}>
-    //         <form>
-                
-    //         </form>
-
-    //     </div>
-    //     <Button label='Upload' />
-    // </div>
