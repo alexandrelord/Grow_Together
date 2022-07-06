34b9e7f6afcfc63cf234299c1e2d7b48b8301eb2
ale lord <j.alexlord@gmail.com>
2022-07-06 11:52:48 -0500

    add css to offscreen menu

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import style from './NavBar.module.css'
 
 export default function NavBar() {
-  const [menu, showMenu] = useState(false)
+  const [menu, showMenu] = useState('offscreen')
   
   return (
-    <nav>
-        <div className={style.elements}>
-            <div className={style.hamburgerMenu} onClick={() => showMenu(!menu) }>
-                <span className={style.dot}></span>
-                <span className={style.dot}></span>
-                <span className={style.dot}></span>
-            </div>
-            <div className={style.title}>
-                <h3>Grow Together</h3>
-                <div className={style.logo}></div>
-            </div>
-        </div>
-    </nav>
+    <>
+      <nav>
+          <div className={style.elements}>
+              <div className={style.hamburgerMenu} onClick={() => showMenu(!menu) }>
+                  <span className={style.dot}></span>
+                  <span className={style.dot}></span>
+                  <span className={style.dot}></span>
+              </div>
+              <div className={style.title}>
+                  <h3>Grow Together</h3>
+                  <div className={style.logo}></div>
+              </div>
+          </div>
+      </nav>
+      <aside className={style.offscreen}>
+        <Link to='/'>Home</Link>
+        <Link to='myplants'>My Plants</Link>
+      </aside>
+    </>
   )
 }
