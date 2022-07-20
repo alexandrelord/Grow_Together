b5586cb55fb167cd9f6dbfa716a5f6f2e41899c8
ale lord <j.alexlord@gmail.com>
2022-07-19 19:34:57 -0500

    matches code written

--- frontend/src/components/Plants/BestMatch.jsx
+++ frontend/src/components/Plants/BestMatch.jsx
@@ -1,14 +1,37 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import useAxiosPrivate from '../../hooks/useAxiosPrivate'
+
+// styles
 import Box from '@mui/material/Box'
 import BestMatchCard from './BestMatchCard'
 
-import { useLocation } from 'react-router-dom'
 
 export default function BestMatch() {
+    const [matches, setMatches] = useState()
+    const axiosPrivate = useAxiosPrivate()
     const location = useLocation()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+      if (matches) navigate('/matches', {state: matches})
+    }, [matches])
+    
+    async function handleClick(e) {
+      e.preventDefault()
+
+      try {
+        const response = await axiosPrivate.post('api/matches/', { light: location.state.plant['light'] })
+        setMatches(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+
+    }
 
     return (
     <Box sx={{ marginTop: 7 }}>
-        <BestMatchCard location={location} />
+        <BestMatchCard location={location} handleClick={handleClick} />
     </Box>
   )
 }
